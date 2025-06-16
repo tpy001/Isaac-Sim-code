6fdb3362a568ca0bc49bb5418e910c34d69594d7
jessapinkman <1784112183@qq.com>
2025-06-16 19:34:51 +0800

    fix bugs

--- umi/test.sh
+++ umi/test.sh
@@ -1,3 +1,2 @@
 source ../catkin_ws/devel/setup.bash
-python3 scripts_sim/inference_umi_with_depth.py  --simple_render False --ckpt_path /home/pinkman/projects/dp_depth/umi/data/outputs/2025.06.02/21.31.12_train_diffusion_unet_timm_umi/checkpoints/best.ckpt --n_action_steps 4
-# python3 scripts_sim/inference_umi.py --ckpt_path ckpt/stack_cube_simv3.ckpt --n_action_steps 4
+python3 scripts_sim/inference_umi_with_depth.py --ckpt_path ckpt/latest.ckpt --n_action_steps 4
